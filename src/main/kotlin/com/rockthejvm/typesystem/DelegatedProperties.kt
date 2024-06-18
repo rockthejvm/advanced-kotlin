a780d324ba977a2becfc591df574f6a78ab57b54
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2024-06-18 13:40:59 +0300

    standard delegates

--- src/main/kotlin/com/rockthejvm/typesystem/DelegatedProperties.kt
+++ src/main/kotlin/com/rockthejvm/typesystem/DelegatedProperties.kt
@@ -1,5 +1,8 @@
 package com.rockthejvm.typesystem
 
+import java.util.UUID
+import kotlin.properties.Delegates
+import kotlin.random.Random
 import kotlin.reflect.KProperty
 
 object DelegatedProperties {
@@ -26,6 +29,23 @@ object DelegatedProperties {
         println(x)
         logger.property = 3
         println(logger.property)
+    }
+
+    class DelegatedProp<A>(constrArgs: Int, default: A) {
+        var store: A = default
+        operator fun getValue(currentRef: Any, prop: KProperty<*>): A {
+            // perform side effect whenever the property is accessed
+            // must return a value of the correct type
+            return store
+        }
+
+        operator fun setValue(currentRef: Any, prop: KProperty<*>, value: A) {
+            // perform side effect whenever the property is changed
+        }
+    }
+
+    class MyUsefulClass {
+        var myProperty: Int by DelegatedProp(constrArgs = 42, default = 0)
     }
 
     // delegated properties
@@ -107,8 +127,122 @@ object DelegatedProperties {
         val y = delayed.delayedProp // no more prints
     }
 
+    /*
+        Standard Delegated Properties
+     */
+
+    // 1 - lazy
+    data class UserData(val name: String, val email: String)
+
+    class Person(val id: String) {
+        private fun fetchUserData(): UserData {
+            // complex or it takes a while
+            println("Fetching user data from remote server...")
+            // simulate something long
+            Thread.sleep(3000)
+            return UserData(name = "John Doe", email = "johndoe@rockthejvm.com")
+        }
+
+        val userData: UserData by lazy { // lazy evaluation - a property is NOT computed until first use
+            // perform long computation or network call
+            // triggered on first use
+            fetchUserData()
+        }
+
+        fun showUserData() {
+            println("User data: $userData")
+        }
+    }
+
+    fun demoLazy() {
+        val person = Person(id = "abc-123")
+        println("user created.") // at this point, fetchUserData() is NOT triggered
+        println("About to show user data...")
+        person.showUserData() // userData is first accessed, fetchUserData() will be triggered
+        // userData is fetched and cached
+        println("Showing user data once more...")
+        person.showUserData() // fetchUserData() will NOT be triggered anymore, userData exists
+    }
+
+    // vetoable
+    class BankAccount(initialBalance: Double) { // NEVER use double for money
+        var balance: Double by Delegates.vetoable(initialBalance) { prop, oldValue, newValue ->
+            // must return a boolean
+            // if true -> var will be changed, if not, the change will be DENIED
+            newValue >= 0
+        }
+    }
+
+    fun demoVeto() {
+        val account = BankAccount(100.0)
+        println("Initial balance: ${account.balance}")
+        account.balance = 150.0 // this should succeed
+        println("Updated balance: ${account.balance}") // 150
+        account.balance = -50.0 // this should be vetoed
+        println("Final balance: ${account.balance}") // 150
+    }
+
+    // observable - perform side effects on changing of your properties
+    // example: monitoring the staleness of a dataset
+    enum class State {
+        NONE, NEW, PROCESSED, STALE
+    }
+
+    class MonitoredDataset(name: String) {
+        var state: State by Delegates.observable(State.NONE) { prop, oldValue, newValue ->
+            // can alert a system if the state changes
+            println("[dataset - $name] State changed: $oldValue -> $newValue")
+            if (newValue == State.STALE)
+                println("[dataset - $name] Alert: dataset is now stale, refresh data")
+        }
+
+        private var data: List<String> = listOf()
+
+        fun consumeData() {
+            if (state == State.PROCESSED)
+                state = State.STALE
+            else if (data.isNotEmpty()) {
+                state = State.PROCESSED
+                // we dump the data to some persistent store
+                data = listOf()
+            }
+        }
+
+        fun fetchData() {
+            if (Random.nextBoolean()) { // data exists upstream
+                data = (1..5).map { UUID.randomUUID().toString() } // get the data
+                state = State.NEW // reset the state
+            }
+        }
+    }
+
+    fun demoObservable() {
+        val dataset = MonitoredDataset("sensor-data-incremental")
+        dataset.fetchData()
+        dataset.consumeData()
+        dataset.fetchData()
+        dataset.consumeData()
+        dataset.consumeData()
+        dataset.consumeData()
+    }
+
+    // map - bridge connection between Kotlin and weakly typed data (e.g. JSON)
+    class WeakObject(val attributes: Map<String, Any>) {
+        val name: String by attributes // this is a delegated property
+        val size: Int by attributes
+    }
+
+    fun demoMapDelegated() {
+        val myDict = WeakObject(mapOf(
+            "size" to 123456,
+            "name" to "Rock the JVM videos"
+        ))
+        println("Name of dataset: ${myDict.name}") // actually uses attributes.get("name") as String,beware this can crash if "name" is not a key in the map
+        println("Size of dataset: ${myDict.size}")
+    }
+
     @JvmStatic
     fun main(args: Array<String>) {
-        demoDelayed()
+        demoMapDelegated()
     }
 }
